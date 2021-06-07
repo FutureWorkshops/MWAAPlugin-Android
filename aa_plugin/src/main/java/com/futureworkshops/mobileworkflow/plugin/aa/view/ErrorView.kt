865b044a0a701da2bf53ec8876d92d16d3264c2d
Igor Castañeda Ferreira <igorcferreira@hotmail.com>
2021-06-07 18:46:53 +0200

    Fix ErrorView

--- aa_plugin/src/main/java/com/futureworkshops/mobileworkflow/plugin/aa/view/ErrorView.kt
+++ aa_plugin/src/main/java/com/futureworkshops/mobileworkflow/plugin/aa/view/ErrorView.kt
@@ -2,20 +2,24 @@ package com.futureworkshops.mobileworkflow.plugin.aa.view
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
 import android.widget.Button
 import android.widget.FrameLayout
-import com.futureworkshops.mobileworkflow.plugin.aa.R
+import com.futureworkshops.mobileworkflow.plugin.aa.databinding.ErrorViewBinding
+import com.futureworkshops.mobileworkflow.ui.part.BindingPart
 
 internal class ErrorView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleRes: Int = 0,
     tintColor: Int?
-) : FrameLayout(context, attrs, defStyleRes) {
+) : FrameLayout(context, attrs, defStyleRes), BindingPart<ErrorViewBinding> {
 
-    private val view: View = View.inflate(context, R.layout.error_view, this)
-    private val retryButton: Button = view.findViewById(R.id.retryButton)
+    constructor(context: Context, attrs: AttributeSet? = null, defStyleRes: Int = 0):
+            this(context, attrs, defStyleRes, null)
+
+    override val view = ErrorViewBinding.inflate(getLayoutInflater(context), this, true)
+    private val retryButton: Button
+        get() = view.retryButton
 
     init {
         tintColor?.apply { retryButton.setBackgroundColor(tintColor) }
