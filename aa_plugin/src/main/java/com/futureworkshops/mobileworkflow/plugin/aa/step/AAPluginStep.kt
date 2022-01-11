297ce0041769a67adf54ac55770d7d12c5c1b5f9
Igor Ferreira <igorcferreira@hotmail.com>
2022-01-11 11:55:51 -0300

    Fix step definition

--- aa_plugin/src/main/java/com/futureworkshops/mobileworkflow/plugin/aa/step/AAPluginStep.kt
+++ aa_plugin/src/main/java/com/futureworkshops/mobileworkflow/plugin/aa/step/AAPluginStep.kt
@@ -5,13 +5,16 @@
 package com.futureworkshops.mobileworkflow.plugin.aa.step
 
 import android.os.Parcelable
+import com.futureworkshops.mobileworkflow.model.configuration.StepLink
 import com.futureworkshops.mobileworkflow.model.step.PluginStep
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class AAPluginStep(
+    override val id: String,
     override val type: String,
     override val title: String,
+    override val links: List<StepLink>,
     val optional: Boolean,
     val licenseURL: String = "",
     val mode: String = ""
