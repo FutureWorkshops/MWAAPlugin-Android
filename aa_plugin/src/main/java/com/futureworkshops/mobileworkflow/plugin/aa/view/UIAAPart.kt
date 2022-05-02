5a9cc9798f953d8b156c2fe5ec6b31530557181c
Tomer Mandel <tomer@futureworkshops.com>
2022-05-02 09:36:43 +0200

    Remove use of StyleablePart and surveyTheme.

--- aa_plugin/src/main/java/com/futureworkshops/mobileworkflow/plugin/aa/view/UIAAPart.kt
+++ aa_plugin/src/main/java/com/futureworkshops/mobileworkflow/plugin/aa/view/UIAAPart.kt
@@ -8,15 +8,13 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.widget.FrameLayout
-import com.futureworkshops.mobileworkflow.SurveyTheme
-import com.futureworkshops.mobileworkflow.backend.views.main_parts.StyleablePart
 import com.futureworkshops.mobileworkflow.plugin.aa.R
 
 class UIAAPart @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleRes: Int = 0
-) : FrameLayout(context, attrs, defStyleRes), StyleablePart {
+) : FrameLayout(context, attrs, defStyleRes) {
 
     val view: View = View.inflate(context, R.layout.aa_plugin_step, this)
 
@@ -27,10 +25,4 @@ class UIAAPart @JvmOverloads constructor(
     private fun createViews() {
 
     }
-
-    override fun style(surveyTheme: SurveyTheme) {
-    }
-
-
-
 }
