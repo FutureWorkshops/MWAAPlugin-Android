a9da2f178081b0175406fef8956091fc016e3ecb
Igor Castañeda Ferreira <me@igorcferreira.dev>
2022-06-16 14:35:05 +0200

    Provide plugin version

--- aa_plugin/src/main/java/com/futureworkshops/mobileworkflow/plugin/aa/AAPluginFactory.kt
+++ aa_plugin/src/main/java/com/futureworkshops/mobileworkflow/plugin/aa/AAPluginFactory.kt
@@ -5,7 +5,13 @@
 package com.futureworkshops.mobileworkflow.plugin.aa
 
 import com.futureworkshops.mobileworkflow.domain.PluginFactory
+import com.futureworkshops.mobileworkflow.model.plugin.PluginInformation
 
 class AAPluginFactory : PluginFactory(
     listOf(AAPlugin())
-)
+) {
+    override fun getInformation(): PluginInformation = PluginInformation(
+        name = BuildConfig.LIBRARY_PACKAGE_NAME,
+        version = BuildConfig.PLUGIN_VERSION
+    )
+}
